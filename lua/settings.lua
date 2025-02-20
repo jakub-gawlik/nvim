119d9e73b2fe7a41be4ef3e1bcc905aa8f9c599e
Jakub Gawlik <info@caasi.dev>
2025-02-20 10:16:18 +0000

    make buffer auto resize toggle-able

--- lua/settings.lua
+++ lua/settings.lua
@@ -48,7 +48,7 @@ vim.opt.smartindent = true
 vim.opt.smarttab = true
 vim.opt.tabstop = 4
 vim.opt.title = true
-vim.opt.wildignore:append({ "*/node_modules/*" })
+vim.opt.wildignore:append({ "*/node_modules/*", "*/vendor/*", "*/.git/*", "*/.DS_Store", "*/tmp/*" })
 vim.opt.wildoptions = "pum"
 vim.opt.winblend = 0
 vim.opt.wrap = false
@@ -84,3 +84,44 @@ vim.cmd([[
     autocmd BufWinEnter * silent! loadview
   augroup END
 ]]) -- Remember folds
+
+-- Toggle buffer auto resize on focus
+local auto_resize_enabled = false
+-- Function to resize window to 80% of total width
+local function resize_to_80_percent()
+	local total_width = vim.o.columns
+	local target_width = math.floor(total_width * 0.8)
+	local win_id = vim.api.nvim_get_current_win()
+
+	-- Get all windows
+	local wins = vim.api.nvim_tabpage_list_wins(0)
+
+	-- First make all windows equal
+	vim.cmd("wincmd =")
+
+	-- Then resize current window
+	vim.api.nvim_win_set_width(win_id, target_width)
+end
+
+-- Function to toggle the auto-resize functionality
+local function toggle_auto_resize()
+	auto_resize_enabled = not auto_resize_enabled
+
+	if auto_resize_enabled then
+		-- Create the autocommand when enabled
+		vim.api.nvim_create_autocmd("WinEnter", {
+			group = vim.api.nvim_create_augroup("ResizeWindow", { clear = true }),
+			callback = function()
+				vim.defer_fn(resize_to_80_percent, 0)
+			end,
+		})
+		print("Auto-resize enabled")
+	else
+		-- Clear the autocommand when disabled
+		vim.api.nvim_clear_autocmds({ group = "ResizeWindow" })
+		print("Auto-resize disabled")
+	end
+end
+
+-- Set up the keybinding (example using <leader>tr for "toggle resize")
+vim.keymap.set("n", "<leader>tr", toggle_auto_resize, { noremap = true, silent = true, desc = "Toggle auto-resize" })
