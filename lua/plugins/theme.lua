fe1c9760b0ac0d72f7ea3431f297cbfea2b8818d
Jakub Gawlik <info@caasi.dev>
2025-03-20 13:08:36 +0000

    change theme to onedark

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,31 +1,46 @@
+-- return {
+-- 	"rebelot/kanagawa.nvim",
+-- 	config = function()
+-- 		require("kanagawa").setup({
+-- 			compile = false,
+-- 			undercurl = true,
+-- 			commentStyle = { italic = true },
+-- 			functionStyle = {},
+-- 			keywordStyle = { italic = true },
+-- 			statementStyle = { bold = true },
+-- 			typeStyle = {},
+-- 			transparent = true,
+-- 			dimInactive = false,
+-- 			terminalColors = true,
+-- 			colors = {
+-- 				palette = {},
+-- 				theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+-- 			},
+-- 			overrides = function(colors)
+-- 				return {}
+-- 			end,
+-- 			theme = "wave",
+-- 			background = {
+-- 				dark = "wave",
+-- 				light = "lotus",
+-- 			},
+-- 		})
+--
+-- 		vim.cmd("colorscheme kanagawa")
+-- 	end,
+-- }
+
 return {
-	"rebelot/kanagawa.nvim",
+	"olimorris/onedarkpro.nvim",
+	priority = 1000,
 	config = function()
-		require("kanagawa").setup({
-			compile = false,
-			undercurl = true,
-			commentStyle = { italic = true },
-			functionStyle = {},
-			keywordStyle = { italic = true },
-			statementStyle = { bold = true },
-			typeStyle = {},
-			transparent = true,
-			dimInactive = false,
-			terminalColors = true,
-			colors = {
-				palette = {},
-				theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-			},
-			overrides = function(colors)
-				return {}
-			end,
-			theme = "wave",
-			background = {
-				dark = "wave",
-				light = "lotus",
+		require("onedarkpro").setup({
+			styles = {
+				comments = "italic",
+				functions = "italic",
 			},
 		})
 
-		vim.cmd("colorscheme kanagawa")
+		vim.cmd("colorscheme onedark")
 	end,
 }
