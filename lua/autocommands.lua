6ed00975ee1f4e28c2a3fe881dbb6fded0e36eea
Jakub Gawlik <info@caasi.dev>
2025-04-24 15:30:03 +0100

    updates

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -5,13 +5,15 @@ api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
 
 -- Enable spell checking for certain file types
 api.nvim_create_autocmd(
-	{ "BufRead", "BufNewFile" },
-	-- { pattern = { "*.txt", "*.md", "*.tex" }, command = [[setlocal spell<cr> setlocal spelllang=en,de<cr>]] }
-	{
-		pattern = { "*.txt", "*.md", "*.tex" },
-		callback = function()
-			vim.opt.spell = true
-			vim.opt.spelllang = "en"
-		end,
-	}
+    { "BufRead", "BufNewFile" },
+    -- { pattern = { "*.txt", "*.md", "*.tex" }, command = [[setlocal spell<cr> setlocal spelllang=en,de<cr>]] }
+    {
+        pattern = { "*.txt", "*.md", "*.tex" },
+        callback = function()
+            vim.opt.spell = true
+            vim.opt.spelllang = "en"
+            vim.opt.formatoptions = 'jcroqlnt'
+            vim.opt.textwidth = 120
+        end,
+    }
 )
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,52 +1,53 @@
--- return {
--- 	"rebelot/kanagawa.nvim",
--- 	config = function()
--- 		require("kanagawa").setup({
--- 			compile = false,
--- 			undercurl = true,
--- 			commentStyle = { italic = true },
--- 			functionStyle = {},
--- 			keywordStyle = { italic = true },
--- 			statementStyle = { bold = true },
--- 			typeStyle = {},
--- 			transparent = true,
--- 			dimInactive = false,
--- 			terminalColors = true,
--- 			colors = {
--- 				palette = {},
--- 				theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
--- 			},
--- 			overrides = function(colors)
--- 				return {}
--- 			end,
--- 			theme = "wave",
--- 			background = {
--- 				dark = "wave",
--- 				light = "lotus",
--- 			},
--- 		})
---
--- 		vim.cmd("colorscheme kanagawa")
--- 	end,
--- }
-
 return {
-	"olimorris/onedarkpro.nvim",
-	priority = 1000,
-	config = function()
-		require("onedarkpro").setup({
-			styles = {
-				comments = "italic",
-				functions = "italic",
-			},
-			colors = {
-				cursorline = "#1a1c22",
-			},
-			options = {
-				cursorline = true,
-			},
-		})
+    "rebelot/kanagawa.nvim",
+    config = function()
+        require("kanagawa").setup({
+            compile = false,
+            undercurl = true,
+            commentStyle = { italic = true },
+            functionStyle = {},
+            keywordStyle = { italic = true },
+            statementStyle = { bold = true },
+            typeStyle = {},
+            transparent = true,
+            dimInactive = false,
+            terminalColors = true,
+            colors = {
+                palette = {},
+                theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+            },
+            overrides = function(colors)
+                return {}
+            end,
+            theme = "wave",
+            background = {
+                dark = "wave",
+                light = "lotus",
+            },
+        })
 
-		vim.cmd("colorscheme onedark")
-	end,
+        vim.cmd("colorscheme kanagawa")
+    end,
 }
+
+-- return {
+--     "olimorris/onedarkpro.nvim",
+--     priority = 1000,
+--     config = function()
+--         require("onedarkpro").setup({
+--             styles = {
+--                 comments = "italic",
+--                 functions = "italic",
+--             },
+--             colors = {
+--                 cursorline = "#1a1c22",
+--             },
+--             options = {
+--                 cursorline = true,
+--                 transparency = true
+--             },
+--         })
+--
+--         vim.cmd("colorscheme onedark")
+--     end,
+-- }
