4750cf43f0c0b8b2c2eea0a38185a25b6b76294d
Jakub Gawlik <info@caasi.dev>
2025-05-02 23:19:19 +0100

    add bufferline, neoscroll and phpactor

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,26 +1,26 @@
 return {
-    "stevearc/conform.nvim",
-    config = function()
-        require("conform").setup({
-            format_on_save = {
-                timeout_ms = 2000,
-                lsp_format = "fallback",
-            },
-            formatters_by_ft = {
-                bash = { "shfmt" },
-                lua = { "stylua" },
-                javascript = { "prettier" },
-                typescript = { "prettier" },
-                vue = { "prettier" },
-                php = { "php-cs-fixer" },
-                markdown = { "prettier" },
-                python = { "black" },
-            },
-            -- formatters = {
-            --     mdformat = {
-            --         prepend_args = { "--wrap", "120" },
-            --     },
-            -- },
-        })
-    end,
+	"stevearc/conform.nvim",
+	config = function()
+		require("conform").setup({
+			format_on_save = {
+				timeout_ms = 2000,
+				lsp_format = "fallback",
+			},
+			formatters_by_ft = {
+				bash = { "shfmt" },
+				lua = { "stylua" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				vue = { "prettier" },
+				php = { "php-cs-fixer" },
+				markdown = { "prettier" },
+				python = { "black" },
+			},
+			-- formatters = {
+			--     mdformat = {
+			--         prepend_args = { "--wrap", "120" },
+			--     },
+			-- },
+		})
+	end,
 }
