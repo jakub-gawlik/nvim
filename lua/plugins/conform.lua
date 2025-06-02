1d7a05d8e78b3a0aa3bcec6e0ac4885e107985bf
Jakub Gawlik <info@caasi.dev>
2025-06-02 20:14:57 +0100

    formatter updates

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,26 +1,28 @@
 return {
-	"stevearc/conform.nvim",
-	config = function()
-		require("conform").setup({
-			format_on_save = {
-				timeout_ms = 2000,
-				lsp_format = "fallback",
-			},
-			formatters_by_ft = {
-				bash = { "shfmt" },
-				lua = { "stylua" },
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				vue = { "prettier" },
-				php = { "php-cs-fixer" },
-				markdown = { "prettier" },
-				python = { "autopep8" },
-			},
-			formatters = {
-				autopep8 = {
-					prepend_args = { "--indent-size", "2" },
-				},
-			},
-		})
-	end,
+  "stevearc/conform.nvim",
+  config = function()
+    require("conform").setup({
+      format_on_save = {
+        timeout_ms = 2000,
+        lsp_format = "fallback",
+      },
+      formatters_by_ft = {
+        bash = { "shfmt" },
+        lua = { "stylua" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        vue = { "prettier" },
+        php = { "php_cs_fixer" },
+        markdown = { "prettier" },
+        python = { "ruff_format" },
+      },
+      formatters = {
+        php_cs_fixer = {
+          command = "php-cs-fixer",
+          args = { "fix", "$FILENAME" },
+          stdin = false,
+        }
+      },
+    })
+  end,
 }
