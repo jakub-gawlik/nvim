fd2d42bee201a702ca2e13dfd773bf70a75ceefa
Jakub Gawlik <info@caasi.dev>
2025-06-02 20:15:08 +0100

    change to tabs

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -3,17 +3,30 @@ local api = vim.api
 -- don't auto comment new line
 api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
 
+-- python specific settings
+api.nvim_create_autocmd(
+  { "FileType" },
+  {
+    pattern = "python",
+    callback = function()
+      vim.opt_local.shiftwidth = 2
+      vim.opt_local.tabstop = 2
+      vim.opt_local.expandtab = true
+    end,
+  }
+)
+
 -- Enable spell checking for certain file types
 api.nvim_create_autocmd(
-    { "BufRead", "BufNewFile" },
-    -- { pattern = { "*.txt", "*.md", "*.tex" }, command = [[setlocal spell<cr> setlocal spelllang=en,de<cr>]] }
-    {
-        pattern = { "*.txt", "*.md", "*.tex" },
-        callback = function()
-            vim.opt.spell = true
-            vim.opt.spelllang = "en"
-            vim.opt.formatoptions = 'jcroqlnt'
-            vim.opt.textwidth = 120
-        end,
-    }
+  { "BufRead", "BufNewFile" },
+  -- { pattern = { "*.txt", "*.md", "*.tex" }, command = [[setlocal spell<cr> setlocal spelllang=en,de<cr>]] }
+  {
+    pattern = { "*.txt", "*.md", "*.tex" },
+    callback = function()
+      vim.opt.spell = true
+      vim.opt.spelllang = "en"
+      vim.opt.formatoptions = 'jcroqlnt'
+      vim.opt.textwidth = 120
+    end,
+  }
 )
--- lua/settings.lua
+++ lua/settings.lua
@@ -37,16 +37,16 @@ vim.opt.shell = "zsh"
 
 -- Disabled for now as it conflicts with telescopes' browser extension plugin and folds files when foldlevel set to 0
 -- Issue raised upstream: https://github.com/nvim-telescope/telescope-file-browser.nvim/issues/249#issuecomment-1471500450
-vim.opt.shiftwidth = 4
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "TelescopeResults",
-	command = "setlocal nofoldenable",
+  pattern = "TelescopeResults",
+  command = "setlocal nofoldenable",
 })
 
 vim.opt.showcmd = true
 vim.opt.smartindent = true
 vim.opt.smarttab = true
-vim.opt.tabstop = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.title = true
 vim.opt.wildignore:append({ "*/node_modules/*", "*/vendor/*", "*/.git/*", "*/.DS_Store", "*/tmp/*" })
 vim.opt.wildoptions = "pum"
@@ -59,8 +59,8 @@ vim.cmd([[let &t_Ce = "\e[4:0m"]])
 
 -- Turn off paste mode when leaving insert
 vim.api.nvim_create_autocmd("InsertLeave", {
-	pattern = "*",
-	command = "set nopaste",
+  pattern = "*",
+  command = "set nopaste",
 })
 
 -- Add asterisks in block comments
@@ -69,58 +69,58 @@ vim.opt.formatoptions:append({ "r" })
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-	group = highlight_group,
-	pattern = "*",
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = "*",
 })
 
 -- Remember folds
 vim.cmd([[
-  augroup remember_folds
-    autocmd!
-    autocmd BufWinLeave * if expand('%:p') !=# '' | mkview | endif
-    autocmd BufWinEnter * silent! loadview
-  augroup END
+	augroup remember_folds
+		autocmd!
+		autocmd BufWinLeave * if expand('%:p') !=# '' | mkview | endif
+		autocmd BufWinEnter * silent! loadview
+	augroup END
 ]]) -- Remember folds
 
 -- Toggle buffer auto resize on focus
 local auto_resize_enabled = false
 -- Function to resize window to 80% of total width
 local function resize_to_80_percent()
-	local total_width = vim.o.columns
-	local target_width = math.floor(total_width * 0.8)
-	local win_id = vim.api.nvim_get_current_win()
+  local total_width = vim.o.columns
+  local target_width = math.floor(total_width * 0.8)
+  local win_id = vim.api.nvim_get_current_win()
 
-	-- Get all windows
-	local wins = vim.api.nvim_tabpage_list_wins(0)
+  -- Get all windows
+  local wins = vim.api.nvim_tabpage_list_wins(0)
 
-	-- First make all windows equal
-	vim.cmd("wincmd =")
+  -- First make all windows equal
+  vim.cmd("wincmd =")
 
-	-- Then resize current window
-	vim.api.nvim_win_set_width(win_id, target_width)
+  -- Then resize current window
+  vim.api.nvim_win_set_width(win_id, target_width)
 end
 
 -- Function to toggle the auto-resize functionality
 local function toggle_auto_resize()
-	auto_resize_enabled = not auto_resize_enabled
+  auto_resize_enabled = not auto_resize_enabled
 
-	if auto_resize_enabled then
-		-- Create the autocommand when enabled
-		vim.api.nvim_create_autocmd("WinEnter", {
-			group = vim.api.nvim_create_augroup("ResizeWindow", { clear = true }),
-			callback = function()
-				vim.defer_fn(resize_to_80_percent, 0)
-			end,
-		})
-		print("Auto-resize enabled")
-	else
-		-- Clear the autocommand when disabled
-		vim.api.nvim_clear_autocmds({ group = "ResizeWindow" })
-		print("Auto-resize disabled")
-	end
+  if auto_resize_enabled then
+    -- Create the autocommand when enabled
+    vim.api.nvim_create_autocmd("WinEnter", {
+      group = vim.api.nvim_create_augroup("ResizeWindow", { clear = true }),
+      callback = function()
+        vim.defer_fn(resize_to_80_percent, 0)
+      end,
+    })
+    print("Auto-resize enabled")
+  else
+    -- Clear the autocommand when disabled
+    vim.api.nvim_clear_autocmds({ group = "ResizeWindow" })
+    print("Auto-resize disabled")
+  end
 end
 
 -- Set up the keybinding (example using <leader>tr for "toggle resize")
